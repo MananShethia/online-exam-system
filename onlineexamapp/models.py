46b7ea8400d30e9fb44e3e4265e3edba3069d8f9
Shethia-Manan <77979535+MananShethia@users.noreply.github.com>
2022-03-16 23:30:43 +0530

    signup user in database

--- onlineexamapp/models.py
+++ onlineexamapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    fname = models.CharField(max_length = 100)
+    lname = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 100)
+    mobile = models.CharField(max_length = 100)
+    gender = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname + ' --> ' + self.email
--- onlineexamapp/views.py
+++ onlineexamapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from onlineexamapp.models import User
 
 # Create your views here.
 def index(request):
@@ -17,7 +18,38 @@ def contact(request):
     return render(request, 'contact.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == "POST":
+        user = User()
+        user.fname = request.POST['fname']
+        user.lname = request.POST['lname']
+        user.email = request.POST['email']
+        user.mobile = request.POST['mobile']
+        user.gender = request.POST['gender']
+        user.address = request.POST['address']
+        # user.save()
+        try:
+            User.objects.get(email = request.POST['email'])
+            msg = 'Email Already Registered'
+            return render(request, 'signup.html', {'msg': msg, 'user': user})
+        except:
+            if request.POST['password'] == request.POST['cpassword']:
+                User.objects.create(
+                    fname = request.POST['fname'],
+                    lname = request.POST['lname'],
+                    email = request.POST['email'],
+                    mobile = request.POST['mobile'],
+                    gender = request.POST['gender'],
+                    password = request.POST['password'],
+                    address = request.POST['address'],
+                )
+                fullName = request.POST['fname'] + ' ' + request.POST['lname']
+                msg = 'Your Account Created Successfully'
+                return render(request, 'login.html', {'msg': msg, 'fullName': fullName})
+            else:
+                msg = 'Password Mismatch'
+                return render(request, 'signup.html', {'msg': msg, 'user': user})
+    else:
+        return render(request, 'signup.html')
 
 def login(request):
     return render(request, 'login.html')
