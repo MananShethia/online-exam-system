a564487b0ee443929ce5f4f10c30b9e0fc5ff011
Shethia-Manan <77979535+MananShethia@users.noreply.github.com>
2022-03-16 21:00:01 +0530

    update load static in index.html

--- onlineexamapp/urls.py
+++ onlineexamapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.support, name='support'),
+    path('', views.about, name='about'),
+    path('', views.blog, name='blog'),
+    path('', views.contact, name='contact'),
 ]
--- onlineexamapp/views.py
+++ onlineexamapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def support(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'index.html')
+
+def blog(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'index.html')
