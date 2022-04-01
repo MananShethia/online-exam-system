ea9a968ee3baafb50e28dd283d9855c3267a434e
MananShethia <mananshethia24@gmail.com>
2022-04-01 21:28:00 +0530

    settings of site update

--- onlineexamsite/settings.py
+++ onlineexamsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%=*kaws9q^zgb$&gx-=j=wk37nhnc5-jz2^@8u$%l$qh)(=u!8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
